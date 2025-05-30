3a7bb6387b4d720f9933afdbe9f208698be6987e
jun10920 <abide0206@naver.com>
2025-05-30 11:18:31 +0900

    Initial commit

---
+++ src/main/java/com/korean/koreanairDiet/KoreanairDietApplication.java
@@ -0,0 +1,13 @@
+package com.korean.koreanairDiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreanairDietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreanairDietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korean/koreanairDiet/KoreanairDietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korean.koreanairDiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreanairDietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
